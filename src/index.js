757049087b452e1c4b1e1cf80c03790fc95e639d
patrick kisyula <pmusyimi42@gmail.com>
2022-12-06 03:26:34 +0300

    Done with deliverable 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 // Your code here
+fetch('http://localhost:3000/characters')
+.then(res=> res.json())
+.then(data => data.map(character => {
+  const allCharacters = document.getElementById('character-bar');
+  const spanText = document.createElement('span')
+  spanText.innerText += character.name;
+  const linky = document.createElement('a');
+  linky.setAttribute('href', `http://localhost:3000/characters/${character.id}`)
+  linky.appendChild(spanText);
+  allCharacters.appendChild(linky);
+  // console.log(allCharacters);
+}))
+
