76bd105357045530576054e3985fa18af07bf429
patrick kisyula <pmusyimi42@gmail.com>
2022-12-07 04:40:26 +0300

    Final commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,68 @@
-// Your code here
+let currentId = 0;
+let existingVotes = 0;
 fetch('http://localhost:3000/characters')
 .then(res=> res.json())
-.then(data => data.map(character => {
+.then(data=> data.map(displayCharacters));
+
+
+const displayCharacters = (character)=>{
   const allCharacters = document.getElementById('character-bar');
-  const spanText = document.createElement('span')
-  spanText.innerText += character.name;
-  const linky = document.createElement('a');
-  linky.setAttribute('href', `http://localhost:3000/characters/${character.id}`)
-  linky.appendChild(spanText);
-  allCharacters.appendChild(linky);
-  // console.log(allCharacters);
-}))
+  const span = document.createElement('span')
+  span.innerText = character.name;
+  span.setAttribute('id', character.id);
+  span.addEventListener('click', (event)=>{
+    // console.log(event.target.id)
+    event.preventDefault();
+    const characterName = document.getElementById('name');
+    characterName.innerText = character.name;
+    const imagy = document.getElementById('image');
+    imagy.src = character.image;
+    const votes = document.getElementById('vote-count');
+    votes.innerText = character.votes;
+    currentId = event.target.id
+    existingVotes = character.votes
+    // console.log(existingVotes)
+  });
+  allCharacters.appendChild(span);
+};
+
+const updateCharacterVotes = (id, votes, added) =>{
+    fetch(`http://localhost:3000/characters/${id}`, {
+      headers: {'Content-Type' : 'application/json'},
+      method: 'PATCH',
+      body: JSON.stringify({
+      votes: (votes + added)
+    })
+    })
+  
+  
+};
+const form = document.getElementById('votes-form')
+  // console.log(vf);
+  form.addEventListener('submit', (event)=>{
+    event.preventDefault();
+    let addedVotes = parseInt(event.target.votes.value)
+    
+    // console.log(addedVotes)
+    // console.log(currentId);
+    updateCharacterVotes(currentId, existingVotes, addedVotes)
+    
+  })
+
+
+const resetVotes = (id) => {
+  fetch(`http://localhost:3000/characters/${id}`, {
+      headers: {'Content-Type' : 'application/json'},
+      method: 'PATCH',
+      body: JSON.stringify({
+      votes: 0
+    })
+    })
+  
+};
+const resetButton = document.getElementById('reset-btn')
+resetButton.addEventListener('click', () =>{
+  resetVotes(currentId)
+})
+
 
